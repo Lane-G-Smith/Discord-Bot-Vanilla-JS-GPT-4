b8b9e9f1bb2a3c3988b1bde024fc1b44d804749a
Lane G Smith <108886772+Lane-G-Smith@users.noreply.github.com>
2023-07-16 09:37:02 -0700

    Update general.js

--- general.js
+++ general.js
@@ -36,7 +36,7 @@ client.on("messageCreate", async function (message) {
         if (message.author.bot) return;
 
 //  must include gpt to trigger a response
-        else if (message.content.toLowerCase().includes("gpt-buddy")) {
+        else if (message.content.toLowerCase().includes("Your-Trigger-Word")) {
 
 // AI personality & response format to user input
         const completion = await openai.createChatCompletion({
